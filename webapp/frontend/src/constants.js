7c707e3f67e6b6535844381c29bd9850e6ea1395
Matthias Brosolo <matthias.brosolo@gmail.com>
2017-08-09 19:06:23 +0200

    add url constants

--- webapp/frontend/src/constants.js
+++ webapp/frontend/src/constants.js
@@ -4,12 +4,23 @@
 (function (angular) {
     'use strict';
 
+    var apiUrl = "/api", api = {url: apiUrl};
     var Environment = {
         debug: true
     };
 
+    api.todos = {url: apiUrl + "todos/"};
+    api.todos.create = api.todos.url;
+    api.todos.get = api.todos.url;
+    api.todos.list = api.todos.url;
+    api.todos.update = api.todos.get;
+    api.todos.delete = api.todos.get;
+    api.todos.list = api.todos.url;
+
+
     angular
         .module('app')
+        .constant('Api', api)
         .constant('Environment', Environment);
 
 })(angular);
