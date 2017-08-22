f8e202c61b560d4f510fcef5d5ba9ee182dbd926
Matthias Brosolo <matthias.brosolo@gmail.com>
2017-08-23 00:04:40 +0200

    setup simple example

--- webapp/default/main.py
+++ webapp/default/main.py
@@ -14,78 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# from google.appengine.api import users
-import json
 import logging
-
 import webapp2
-
-import models
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-class BaseHandler(webapp2.RequestHandler):
-    def dispatch(self):
-        # inject headers here (self.request.headers)
-        super(BaseHandler, self).dispatch()
-
-
-class MainHandler(webapp2.RequestHandler):
+class HelloWorldHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 
-class TodoHandler(webapp2.RequestHandler):
-    def get(self):
-        todos = models.Todo.query().fetch()
-        self.response.set_status(200)
-        # logger.info('%s %s', todos[0].key, todos[0].key.urlsafe())
-        payload = []
-        for todo in todos:
-            data = todo.to_dict()
-            data['id'] = todo.key.urlsafe()
-            payload.append(data)
-        self.response.write(json.dumps(payload))
-
-    def post(self):
-        payload = self.request.body
-        data = json.loads(payload)
-        logger.debug(data)
-
-        # todo = models.Todo()
-        todo = models.Todo(**data)
-        todo.put()
-        self.response.set_status(201)
-        self.response.write(json.dumps(todo.to_dict()))
-
-
-class TodoDetailsHandler(webapp2.RequestHandler):
-    def delete(self, todo_id):
-        self.response.set_status(200)
-        self.response.write('Not Implemented Yet')
-
-    def get(self, todo_id):
-        todo = models.Todo.get(todo_id)
-        payload = todo.to_dict()
-        payload['id'] = todo.key.urlsafe()
-        self.response.set_status(200)
-        self.response.write(json.dumps(payload))
-
-    def patch(self, todo_id):
-        self.response.set_status(200)
-        self.response.write('Not Implemented Yet')
-
-    def put(self, todo_id):
-        self.response.set_status(200)
-        self.response.write('Not Implemented Yet')
-
-
 APPLICATION_HANDLERS = [
-    (r'/api/', MainHandler),
-    (r'/api/todos/', TodoHandler),
-    (r'/api/todos/(.*)/', TodoDetailsHandler),
+    (r'/api/', HelloWorldHandler),
 ]
 
 app = webapp2.WSGIApplication(APPLICATION_HANDLERS, debug=True)
--- webapp/frontend/src/app.module.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by matthias on 17/07/17.
- */
-
-(function (angular) {
-    "use strict";
-    angular.module('app', []);
-})(angular);
